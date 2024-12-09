0afc6bf18274a23dfecd33810ea8b77465b57a8f
Kamlesh Baldaniya <kamleshbaldaniya121@gmail.com>
2024-12-09 14:22:06 +0530

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  content: [""], // Adjust paths based on your file structure
+  theme: {
+    extend: {  fontSize: {
+      'xxs': '2px', // Extra extra small
+      'custom-lg': '22px', // Custom large size
+    },
+
+    },
+  },
+  plugins: [],
+};
